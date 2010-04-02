da816d40fc564ef16a2bf9f1cadf8ce707b96d3f
Zack Hobson <zack@zackhobson.com>
2010-04-01 21:30:43 -0700

    added twitter streaming to clients using twitter-node

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
-Dependencies:
+Streams twitter messages to web browsers in real time using web sockets.
+
+## Dependencies:
 
 * [node.js][1]
 * [fab v3][2]
 * [node.ws.js][3]
+* [twitter-node][4]
 
-For more info see my [blog post][4] on the subject.
+## Usage
+
+For more info see my [blog post][5] on the subject.
 
 [1]: http://github.com/ry/node
 [2]: http://github.com/jed/fab/tree/v3
 [3]: http://github.com/ncr/node.ws.js
-[4]: http://zackhobson.com/2010/03/28/node-js-and-web-sockets.html
+[4]: http://github.com/technoweenie/twitter-node
+[5]: http://zackhobson.com/2010/03/28/node-js-and-web-sockets.html
 
 
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// usage: env user=USER password=PASSWORD node server.js <term1> <term2>
 var
   http = require("http"),
   sys  = require("sys"),
@@ -5,6 +6,7 @@ var
   ws   = require("./vendor/ws"),
   fab  = require("./vendor/fab"),
   gad  = require("./vendor/gad"),
+  tn   = require("./vendor/twitter-node"),
 
   app  = fab()
     (fab.listener)
@@ -13,21 +15,46 @@ var
     ('/', gad.file_path('./public/canvas.html'))
   (),
 
+  client_sockets = {},
+  client_count   = 0,
+
   game_server = ws.createServer(function (socket) {
+    var socket_id = "client" + client_count
+    client_sockets[socket_id] = socket
+    client_count += 1
+
     socket.addListener("connect", function (resource) {
-      sys.puts("client connected from " + resource)
-      socket.write("welcome\r\n")
+      sys.puts("client " + socket_id + " connected from " + resource)
     })
     socket.addListener("data", function (data) {
-      socket.write(data)
+      sys.puts("client sent " + sys.inspect(data))
     })
     socket.addListener("close", function () {
-      sys.puts("client left the game")
+      delete client_sockets[socket_id]
+      sys.puts("client left the session")
     })
+  }),
+
+  twitter = new tn.TwitterNode({
+    user: process.env['user'],
+    password: process.env['password'],
+    track: ['ipad']
   })
 
 http.createServer(app).listen(0xFAB) // 4011
 game_server.listen(3840) // 3840
+twitter.addListener('tweet', function (tweet) {
+  sys.puts("got " + sys.inspect(tweet.text) + " from stream")
+  for (socket_id in client_sockets) {
+    if (client_sockets[socket_id]) {
+      client_sockets[socket_id].write(tweet.text)
+    }
+  }
+})
+.addListener('close', function (response) {
+  sys.puts('twitter connection closed.')
+  sys.puts(sys.inspect(response))
+}).stream()
 
 require("repl").start("> ")
 
